7513277d2779336e5241c3ef7337c67aa099ad4e
minuda <min9288@gmail.com>
2022-07-27 04:07:26 +0900

    CREATE : execption 생성

---
+++ board/src/main/java/com/board/exception/EmailNotAuthenticatedException.java
@@ -0,0 +1,14 @@
+package com.board.exception;
+
+public class EmailNotAuthenticatedException extends RuntimeException {
+    public EmailNotAuthenticatedException() {
+    }
+
+    public EmailNotAuthenticatedException(String message) {
+        super(message);
+    }
+
+    public EmailNotAuthenticatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ board/src/main/java/com/board/exception/advise/ExceptionAdvice.java
@@ -0,0 +1,67 @@
+package com.board.exception.advise;
+
+import com.board.domain.response.service.ResponseService;
+import com.board.domain.result.Result;
+import com.board.exception.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(MemberEmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result userEmailAlreadyExistsException() {
+        return responseService.getFailureResult(-101, "이미 존재하는 이메일입니다.");
+    }
+
+    @ExceptionHandler(LoginFailureException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result loginFailureException() {
+        return responseService.getFailureResult(-102, "아이디 혹은 비밀번호가 틀립니다.");
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result memberNotFoundException() {
+        return responseService.getFailureResult(-103, "회원정보를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(EmailAuthTokenNotFountException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result emailAuthTokenNotFountException() {
+        return responseService.getFailureResult(-104, "유효하지 않은 인증요청입니다.");
+    }
+
+    @ExceptionHandler(EmailNotAuthenticatedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result emailAuthenticationException() {
+        return responseService.getFailureResult(-105, "이메일 인증이 필요합니다.");
+    }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result invalidRefreshTokenException() {
+        return responseService.getFailureResult(-106, "Refresh Token이 유효하지 않습니다.");
+    }
+
+    @ExceptionHandler(AuthenticationEntryPointException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result authenticationEntryPointException() {
+        return responseService.getFailureResult(-107, "인증 실패에 따른 예외가 발생했습니다.");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result accessDeniedException() {
+        return responseService.getFailureResult(-108, "인가에 따른 예외가 발생했습니다.");
+    }
+
+}
