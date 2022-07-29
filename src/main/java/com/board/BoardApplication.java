8babfedfb39d3264e9e7d8df0ffbcae85838b18e
minuda <min9288@gmail.com>
2022-07-29 22:56:16 +0900

    UPDATE : 무중단 배포용 설정파일 수정

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -2,16 +2,29 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/Member_Board_REST-API/real-application.yml";
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(BoardApplication.class, args);
+//    }
+
     public static void main(String[] args) {
-        SpringApplication.run(BoardApplication.class, args);
+        new SpringApplicationBuilder(BoardApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                        .run(args);
     }
 
     @Bean
--- src/main/java/com/board/security/SecurityConfig.java
+++ src/main/java/com/board/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sign/**").permitAll()
                 .antMatchers("/exception/**").permitAll()
                 .antMatchers("/confirm-email").permitAll()
-                .antMatchers("/test").permitAll()
+                .antMatchers("/test/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
