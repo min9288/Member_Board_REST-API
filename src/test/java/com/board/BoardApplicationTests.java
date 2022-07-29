a908fc7a59a762197f90f9efa50a807502c0e6a7
minuda <min9288@gmail.com>
2022-07-29 23:04:35 +0900

    UPDATE : 무중단 배포용 설정파일 재수정

--- src/test/java/com/board/BoardApplicationTests.java
+++ src/test/java/com/board/BoardApplicationTests.java
@@ -1,7 +1,9 @@
 package com.board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 class BoardApplicationTests {
